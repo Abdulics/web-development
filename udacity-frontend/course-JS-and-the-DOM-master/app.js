f8f971c52f3a961671fca8e4edf97ca49b107bf3
Abdulics <Abdulics93@yahoo.com>
2023-06-22 10:52:13 -0500

    Nothing too crazy here

--- udacity-frontend/course-JS-and-the-DOM-master/app.js
+++ udacity-frontend/course-JS-and-the-DOM-master/app.js
@@ -1,2 +1,11 @@
-const cardN = document.querySelector('.card');
-cardN.textContent = 'Everything to know about data';
+const element1 = document.querySelector('h1');
+element1.addEventListener('mouseover', function(){
+   element1.textContent = 'Build an AR app';
+});
+
+element1.addEventListener('mouseout', function () {
+    element1.textContent = 'Learn ARKit';
+  });
+
+
+
--- udacity-frontend/lab/toDo-list-app/assets/js/index.js
+++ udacity-frontend/lab/toDo-list-app/assets/js/index.js
@@ -1,19 +1,3 @@
-const list = document.createDocumentFragment();
-const ul = document.createElement('ul');
-const li = document.createElement('li');
-list.appendChild(ul);
-
-const button = document.querySelector('.plus-btn');
-button.addEventListener('click', function(e){
-    const item = document.querySelector('#task');
-    console.log('inner test is: ' +item.textContent);
-});
-
-const input = document.querySelector('input');
-
-
-input.addEventListener('input', myFunc(e) );
-ul.appendChild(li);
-function myFunc(e){
-    
-} 
+let list = document.createDocumentFragment();
+let ul = document.createElement('ul');
+console.log('something...');
